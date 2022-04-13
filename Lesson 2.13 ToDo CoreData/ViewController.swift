787d930c732c753a6fdc63ca55785baa70dc3dac
daewon2020 <daewon@yandex.ru>
2022-04-13 16:24:58 +0300

    Initial Commit

---
+++ Lesson 2.13 ToDo CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.13 ToDo CoreData
+//
+//  Created by Kostya on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
